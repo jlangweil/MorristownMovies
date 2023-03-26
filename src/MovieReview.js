1d732e2592937fc55d9af8bb43d37ace2bb32967
Joshua Langweil <jlangweil@gmail.com>
2023-03-26 14:53:55 -0400

    Added filter and infinite scroll

--- src/MovieReview.js
+++ src/MovieReview.js
@@ -1,82 +1,92 @@
-/* import React from 'react';
-import './MovieReview.css';
-
-const MovieReview = ({ movieName, reviewText, userName, numberOfStars, dateOfReview }) => {
-  const stars = [];
-  for (let i = 1; i <= Math.floor(numberOfStars); i++) {
-    stars.push(<i className="fas fa-star full-star" key={i}></i>);
-  }
-
-  if (numberOfStars % 1 !== 0) {
-    stars.push(<i className="fas fa-star-half-alt half-star" key="half"></i>);
-  }
-
-  return (
-    <div className="movie-review">
-      <h2 className="movie-name">{movieName}</h2>
-      <p className="review-text">{reviewText}</p>
-      <p className="user-name">Reviewed by {userName}</p>
-      <p className="review-date">Date: {dateOfReview}</p>
-      <div className="rating">{stars}</div>
-    </div>
-  );
-};
-
-export default MovieReview; */
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import './MovieReview.css';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import FilterDropdown from './components/Utils/FilterDropdown';
 
 const MovieReview = () => {
-  const [reviews, setReviews] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const [reviews, setReviews] = useState([]);
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [hasMore, setHasMore] = useState(true);
+    const [page, setPage] = useState(1);
+    const [filter, setFilter] = useState("");
+  
+    const fetchMovies = useCallback(async () => {
+        try {
+          const response = await axios.get('https://movies-six-gilt.vercel.app/api/movies');
+          setMovies(response.data);
+        } catch (error) {
+          console.error(error);
+        }
+      }, []);
+    
+      useEffect(() => {
+        fetchMovies();
+      }, [fetchMovies]);
 
-  useEffect(() => {
-    axios.get('https://movies-six-gilt.vercel.app/api/reviews')
-      .then(response => {
-        setReviews(response.data);
-        setLoading(false);
-      })
-      .catch(error => {
-        setError(error);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  return (
-    <div>
-      {reviews.map(review => {
-        return (
-          <div className="movie-review" key={review.id}>
+      const fetchReviews = useCallback(async () => {
+        try {
+          const response = await axios.get(`https://movies-six-gilt.vercel.app/api/reviews?page=${page}&limit=10&movie=${encodeURIComponent(filter)}`);
+          if (response.data.length === 0) {
+            setHasMore(false);
+          } else {
+            setReviews(prevReviews => (page === 1 ? response.data : [...prevReviews, ...response.data]));
+            setPage(prevPage => prevPage + 1);
+          }
+          setLoading(false);
+        } catch (error) {
+          setError(error);
+          setLoading(false);
+        }
+      }, [page, filter]);
+    
+      useEffect(() => {
+        setPage(1);
+        fetchReviews();
+      }, [filter]);
+      
+    
+      if (loading) {
+        return <div>Loading...</div>;
+      }
+    
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      }
+    
+      return (
+        <>
+        <FilterDropdown movies={movies} setFilter={setFilter} />
+        <InfiniteScroll
+        dataLength={reviews.length}
+        next={fetchReviews}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+        endMessage={<p>End of Reviews</p>}
+        >
+        {reviews
+        .filter(review => (filter === '' || review.MovieName === filter))
+        .map(review => (
+            <div className="movie-review" key={review.id}>
             <h2 className="movie-name">{review.MovieName}</h2>
             <p className="review-text">{review.ReviewText}</p>
             <p className="user-name">Reviewed by {review.UserName}</p>
             <p className="review-date">Date: {review.DateOfReview}</p>
             <div className="rating">
-              {Array.from({ length: Math.floor(review.Rating) }, (_, index) => (
+                {Array.from({ length: Math.floor(review.Rating) }, (_, index) => (
                 <i className="fas fa-star full-star" key={index}></i>
-              ))}
-              {review.Rating % 1 !== 0 && (
+                ))}
+                {review.Rating % 1 !== 0 && (
                 <i className="fas fa-star-half-alt half-star" key="half"></i>
-              )}
+                )}
             </div>
-          </div>
-        );
-      })}
-    </div>
-  );
-  
-};
+            </div>
+        ))}
+        </InfiniteScroll>
+        </>
+    );
+    };
 
 export default MovieReview;
-
---
+++ src/components/Utils/FilterDropdown.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Select from 'react-select';
+import './FilterDropdown.css';
+
+const customStyles = {
+  control: (provided) => ({
+    ...provided,
+    backgroundColor: 'black',
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    color: 'white',
+    fontWeight: state.isSelected || state.data.className === 'all-movies-option' ? 'bold' : 'normal',
+    fontStyle: state.isSelected || state.data.className === 'all-movies-option' ? 'italic' : 'normal',
+  }),
+  singleValue: (provided, state) => ({
+    ...provided,
+    color: 'white',
+    fontWeight: state.data.className === 'all-movies-option' ? 'bold' : 'normal',
+    fontStyle: state.data.className === 'all-movies-option' ? 'italic' : 'normal',
+  }),
+  menu: (provided) => ({
+    ...provided,
+    backgroundColor: 'black',
+  }),
+};
+
+const FilterDropdown = ({ setFilter }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get('https://movies-six-gilt.vercel.app/api/movies');
+        setMovies(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  const movieOptions = [
+    { value: '', label: 'All Movies', className: 'all-movies-option' },
+    ...movies.map(movie => ({ value: movie.MovieName, label: movie.MovieName })),
+  ];
+
+  const handleChange = (selectedOption) => {
+    setFilter(selectedOption.value);
+  };
+
+  return (
+    <div className="filter-dropdown">
+      <Select
+        options={movieOptions}
+        onChange={handleChange}
+        styles={customStyles}
+      />
+    </div>
+  );
+};
+
+export default FilterDropdown;
