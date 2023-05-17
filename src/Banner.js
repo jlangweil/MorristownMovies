abca3dad9db634b5d740fc2c20e291b3eed420b8
Joshua Langweil <jlangweil@gmail.com>
2023-05-17 14:41:41 -0400

    profile menu

--- src/Banner.js
+++ src/Banner.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import UserProfileIcon from './components/UserProfile/UserProfileIcon';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
@@ -9,6 +9,11 @@ function Banner({ onButtonClick }) {
   const { currentUser, logout } = useAuth();
   const loggedIn = currentUser !== null;
   const navigate = useNavigate();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  }
  
 
   const handleLogout = async () => {
@@ -18,42 +23,20 @@ function Banner({ onButtonClick }) {
 
   return (
     <>
-    <Container fluid>
-      <Row className="align-items-center">
-        <Col xl={2} className="text-end">
-         
-        </Col>
-        <Col xl={8} className="text-center">
-            <div className="banner-text">
-              <Link to="/" className="text-decoration-none text-reset">
-                 The Morristown Movie <span className="default-font">&</span> Dining Meetup Group
-            </Link>
-        </div>
-        </Col>
-        {/* <Col xl={2} className="text-end">
-         
-        </Col> */}
-        {/* <Col xl={2} className="d-flex justify-content-xl-end justify-content-center">
-          <div className="auth-buttons d-flex align-items-center">
-            {loggedIn && (
-              <UserProfileIcon initial={currentUser[0]} />
-            )}
-            {!loggedIn && (
-              <Link to="/signup">
-                <Button variant="primary" className="responsive-text">Sign Up</Button>
-              </Link>
-            )}
-            <Button
-              variant="secondary"
-              onClick={loggedIn ? handleLogout : () => navigate('/login')}
-            >
-              {loggedIn ? 'Logout' : 'Login'}
-            </Button>
-            
-          </div>
-        </Col> */}
-      </Row>
-      <Row className="d-none d-lg-flex bg-darkred py-2">
+  <Container fluid>
+    <Row className="align-items-center">
+      <Col xl={2} className="text-end">
+        
+      </Col>
+      <Col xl={8} className="text-center">
+          <div className="banner-text">
+            <Link to="/" className="text-decoration-none text-reset">
+                The Morristown Movie <span className="default-font">&</span> Dining Meetup Group
+          </Link>
+      </div>
+      </Col>
+  </Row>
+  <Row className="d-none d-lg-flex bg-darkred py-2">
   <Col className="text-center">
     <NavLink exact to="/" className="menu-link">Home</NavLink>
   </Col>
@@ -81,7 +64,17 @@ function Banner({ onButtonClick }) {
     </Container>
     <div className="auth-buttons pinned-hamburger">
     {loggedIn && (
-              <Link to="/user"><UserProfileIcon initial={currentUser[0]} showArrow /></Link>
+              // <Link to="/user"><UserProfileIcon initial={currentUser[0]} showArrow /></Link>
+              <div onClick={toggleDropdown}>
+              <UserProfileIcon initial={currentUser[0]} showArrow />
+              {dropdownOpen && (
+                <div className="user-menu">
+                  <Link to="/user">Profile</Link>
+                  <br/>
+                  <Link to="/members">Members</Link>
+                </div>
+              )}
+            </div>
             )}
             {!loggedIn && (
               <Link to="/signup">
